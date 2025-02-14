c6a2cef4d6df1dd1f509ecea62a3f714c239f33f
seera <now-kirill@ya.ru>
2025-02-14 16:57:48 +0300

    06-greedy-algorithms.6.2-spices.01-spices

---
+++ 06-greedy-algorithms/6.2-spices/01-spices/code/A11.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class A11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] spicesCountAndBagVolume = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(spicesCountAndBagVolume[0]);
+        long W = Long.parseLong(spicesCountAndBagVolume[1]);
+        Queue<Spice> spices = new PriorityQueue<>(n);
+        for (int i = 0; i < n; i++) {
+            long[] arr = Arrays.stream(scanner.nextLine().split(" ")).mapToLong(Long::parseLong).toArray();
+            spices.add(new Spice(arr[0], arr[1]));
+        }
+        scanner.close();
+        double result = 0.0;
+        while (!spices.isEmpty() && W > 0) {
+            Spice spice = spices.poll();
+            long amount = Math.min(spice.Weight, W);
+            result += spice.costByItem * amount;
+            W -= amount;
+        }
+        System.out.printf(Locale.US, "%.3f%n", result);
+    }
+
+
+    public static class Spice implements Comparable<Spice> {
+
+        private final long Cost;
+
+        private final long Weight;
+
+
+        private final double costByItem;
+
+        public Spice(long cost, long weight) {
+            this.Cost = cost;
+            this.Weight = weight;
+            this.costByItem = (double) cost / weight;
+        }
+
+        @Override
+        public int compareTo(Spice o) {
+            return Double.compare(o.costByItem, this.costByItem);
+        }
+
+    }
+}
