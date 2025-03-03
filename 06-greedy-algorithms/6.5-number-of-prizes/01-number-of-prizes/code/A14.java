9623c90c820d4116bf804cf93c9f013e172f71f1
seera <now-kirill@ya.ru>
2025-03-03 12:02:55 +0300

    06-greedy-algorithms.6.5-number-of-prizes.01-number-of-prizes.

---
+++ 06-greedy-algorithms/6.5-number-of-prizes/01-number-of-prizes/code/A14.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class A14 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        int result = 0;
+        for (int i = (int) Math.max(1, Math.pow(n, 0.5) - 2); i < n; i++) {
+            int x = (i * (i +1)) / 2;
+            if (x > n) {
+                break;
+            }
+            result = i;
+        }
+        System.out.println(result);
+    }
+}
