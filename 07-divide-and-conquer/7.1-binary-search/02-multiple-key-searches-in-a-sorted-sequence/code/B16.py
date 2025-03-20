949388ef8a232bd8fe525ef103298ecac08c6311
seera <now-kirill@ya.ru>
2025-03-20 15:58:11 +0300

    07-divide-and-conquer.7.1-binary-search.02-multiple-key-searches-in-a-sorted-sequence.

---
+++ 07-divide-and-conquer/7.1-binary-search/02-multiple-key-searches-in-a-sorted-sequence/code/B16.py
@@ -0,0 +1,25 @@
+def many_bin_search(n, K, m, Q):
+    result = []
+    for i in (Q):
+        min = 0
+        max = n - 1
+        res = -1
+        while max >= min:
+            mid = (min + max) // 2
+            if K[mid] == i:
+                res = mid
+                break
+            elif K[mid] > i:
+                max = mid - 1
+            else:
+                min = mid + 1
+        result.append(res)
+    return result
+
+n = int(input())
+K = list(map(int, input().split()))
+m = int(input())
+Q = list(map(int, input().split()))
+
+for i in many_bin_search(n, K, m, Q):
+    print(i)
