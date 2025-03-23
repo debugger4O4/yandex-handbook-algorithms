a6f6c1f8e64be40a536cfbed160b7701d9e8198d
seera <now-kirill@ya.ru>
2025-03-23 06:59:27 +0300

    07-divide-and-conquer.7.1-binary-search.03-counting-the-number-of-occurrences-of-elements.

--- 07-divide-and-conquer/7.1-binary-search/03-counting-the-number-of-occurrences-of-elements/code/C16.py
+++ 07-divide-and-conquer/7.1-binary-search/03-counting-the-number-of-occurrences-of-elements/code/C16.py
@@ -1,40 +1,39 @@
-def bin_search(n_k, arr_k, m_q, arr_q):
-    final = []
-    for number_q in arr_q:
-        min_index = 0
-        max_index = n_k - 1
-        result = 0
-        min_index_seq = -1
-        while max_index >= min_index:
-            mid_index = (max_index + min_index) // 2
-            if arr_k[mid_index] == number_q:
-                max_index = mid_index - 1
-                min_index_seq = mid_index
-            elif arr_k[mid_index] > number_q:
-                max_index = mid_index - 1
+def solution(n, K, m, Q):
+    result = []
+    for i in Q:
+        min_idx = 0
+        max_idx = n - 1
+        min_seq = -1
+        while max_idx >= min_idx:
+            mid_idx = (max_idx + min_idx) // 2
+            if K[mid_idx] == i:
+                max_idx = mid_idx - 1
+                min_seq = mid_idx
+            elif K[mid_idx] > i:
+                max_idx = mid_idx - 1
             else:
-                min_index = mid_index + 1
-        min_index = min_index_seq
-        max_index = n_k - 1
-        max_index_seq = min_index_seq
-        while min_index_seq != -1 and max_index >= min_index:
-            mid_index = (max_index + min_index) // 2
-            if arr_k[mid_index] == number_q:
-                min_index = mid_index + 1
-                max_index_seq = mid_index
-            elif arr_k[mid_index] > number_q:
-                max_index = mid_index - 1
+                min_idx = mid_idx + 1
+        min_idx = min_seq
+        max_idx = n - 1
+        max_seq = min_seq
+        while min_seq != -1 and max_idx >= min_idx:
+            mid_idx = (max_idx + min_idx) // 2
+            if K[mid_idx] == i:
+                min_idx = mid_idx + 1
+                max_seq = mid_idx
+            elif K[mid_idx] > i:
+                max_idx = mid_idx - 1
             else:
-                min_index = mid_index + 1
-        if min_index_seq == -1:
-            final.append(0)
+                min_idx = mid_idx + 1
+        if min_seq == -1:
+            result.append(0)
         else:
-            final.append(max_index_seq - min_index_seq + 1)
-    return final
+            result.append(max_seq - min_seq + 1)
+    return result
 
 n = int(input())
-k = list(map(int, input().split()))
+K = list(map(int, input().split()))
 m = int(input())
-q = list(map(int, input().split()))
+Q = list(map(int, input().split()))
 
-print(*bin_search(n, k, m, q))
+print(*solution(n, K, m, Q))
